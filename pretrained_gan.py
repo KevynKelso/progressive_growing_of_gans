799726a7e80e4a5904b1a323ba0c72f8afa704a6
Kevyn Kelso <kevynkelso@outlook.com>
2023-11-14 07:50:23 -0700

    feat: release resources on error

--- pretrained_gan.py
+++ pretrained_gan.py
@@ -17,26 +17,33 @@ class TF1Unpickler(pickle.Unpickler):
 
 
 def run_model(pickle_file, latents):
-    # Initialize TensorFlow session.
-    tf.InteractiveSession()
+    try:
+        # Initialize TensorFlow session.
+        tf.InteractiveSession()
 
-    # Import official CelebA-HQ networks.
-    with open(pickle_file, "rb") as file:
-        _, _, Gs = TF1Unpickler(file).load()
+        # Import official CelebA-HQ networks.
+        with open(pickle_file, "rb") as file:
+            _, _, Gs = TF1Unpickler(file).load()
 
-    # Generate dummy labels (not used by the official networks).
-    labels = np.zeros([latents.shape[0]] + Gs.input_shapes[1][1:])
+        # Generate dummy labels (not used by the official networks).
+        labels = np.zeros([latents.shape[0]] + Gs.input_shapes[1][1:])
 
-    # Run the generator to produce a set of images.
-    images = Gs.run(latents, labels)
+        # Run the generator to produce a set of images.
+        images = Gs.run(latents, labels)
 
-    # Convert images to PIL-compatible format.
-    images = np.clip(np.rint((images + 1.0) / 2.0 * 255.0), 0.0, 255.0).astype(
-        np.uint8
-    )  # [-1,1] => [0,255]
-    images = images.transpose(0, 2, 3, 1)  # NCHW => NHWC
+        # Convert images to PIL-compatible format.
+        images = np.clip(np.rint((images + 1.0) / 2.0 * 255.0), 0.0, 255.0).astype(
+            np.uint8
+        )  # [-1,1] => [0,255]
+        images = images.transpose(0, 2, 3, 1)  # NCHW => NHWC
 
-    return images
+        tf.InteractiveSession().close()
+
+        return images
+    except Exception as e:
+        tf.InteractiveSession().close()
+        raise e
+
 
 
 if __name__ == "__main__":
